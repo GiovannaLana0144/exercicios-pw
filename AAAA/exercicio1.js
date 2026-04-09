eccb7aa56fc1f461e07a6e47593afefd54f2f3ca
GiovannaLana0144 <giovannalana0144@gmail.com>
2026-04-08 21:59:51 -0300

    Add files via upload

---
+++ AAAA/exercicio1.js
@@ -0,0 +1,10 @@
+let x = 1
+
+
+while (x <20){
+x+=1
+console.log (x)
+}
+
+
+
---
+++ AAAA/exercicio2.js
@@ -0,0 +1,15 @@
+let input = require ('readline-sync');
+
+let x = input.questionInt ('digite seu numero');
+
+while (x <20)
+x+=1
+if(x <20){
+    console.log (x)
+
+}
+
+
+
+
+
